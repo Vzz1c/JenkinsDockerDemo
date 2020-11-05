4ad929172c5ae4160511c3190c4f3d2d7f2f3d36
Vzz1c <763717310@qq.com>
2020-11-05 17:08:34 +0800

    1111

---
+++ src/main/java/com/demo/JenkinsDocker01.java
@@ -0,0 +1,18 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JenkinsDocker01 {
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsDocker01.class);
+    }
+    @GetMapping("/")
+    public String getString(){
+        return "JenkinsDocker01";
+    }
+}
